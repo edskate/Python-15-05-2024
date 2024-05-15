3ffc308c13c12c9ad5b5697723bf3ebe5a0a98d8
edlson gomes de lima <edskateicevida@gmail.com>
2024-05-15 08:59:19 -0300

    add ok

---
+++ variavais.py
@@ -0,0 +1,27 @@
+media = 0
+n1 = n2 = n3 = n4 = 0.0
+nome, idade = 'edy', 47
+stado = True
+ 
+ 
+# Funçao type() para verificar o tipo da variável
+print(type(media))
+print(type(n1))
+print(type(n2))
+print(type(n3))
+print(type(n4))
+print(type(nome))
+print(type(idade))
+print(type(stado))
+print(type(1+2j))
+
+
+# Função isinstance 
+a = 10
+b = 'sol'
+print(isinstance(a,(int, float)))
+
+a = 40
+c = 3
+r = a * c
+print(r)
