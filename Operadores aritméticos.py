0f37642bce092cef87fe8958d7c1e49d208651e4
edlson gomes de lima <edskateicevida@gmail.com>
2024-05-15 17:03:14 -0300

    Operedores arentimeticos

---
+++ Operadores aritméticos.py
@@ -0,0 +1,39 @@
+x = y = z = 0
+x = 7
+y = 9
+
+z = x + y
+
+print('A soma de dois valores é ', z)
+
+
+x = y = z = 10
+
+x = 10
+y = 20
+
+z = x - y
+
+print('A subtração de dois valores é ', z)
+
+y = 10
+x = 20
+
+z = x * y
+
+print('A multiplicação de dois valores é ', z)
+
+
+y = 10
+x = 20
+
+z = x / y
+
+print('A divisão de dois valores é ', z)
+
+y = 30
+x = 40
+
+z = x % y
+
+print('O resto da divisão de dois valores é ', z)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,16 @@
+
+y = 9
+
+z = x + y
+
+print('A soma de dois valores é ', z)
+
+
+x = y = z = 10
+
+x = 30
+y = 20
+
+z = x - y
+
+print('A subtração de dois valores é ', z)
